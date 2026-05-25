9190ba476be9ebea7dcf56fedcda55682dd59c13
Khushikhan2419 <firdosk020@gmail.com>
2026-05-25 22:59:56 +0530

    Update database configuration and restore app functionality

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ CORS(app)
 
 # ---------------- DATABASE CONFIG ----------------
 db_config = {
-    "host": "database-1.czockkuoo2fx.ap-northeast-1.rds.amazonaws.com",
+    "host": "database-2.czockkuoo2fx.ap-northeast-1.rds.amazonaws.com",
     "user": "admin",
     "password": "cloud123",
     "database": "cloud"
